132fcd2583011c950b7595624bbcbf99e4f77f34
Joe Sorensen <hello@murl.digital>
2021-10-08 09:52:58 -0600

    added logic to load commands and points

--- src/main/java/digital/murl/aurora/Plugin.java
+++ src/main/java/digital/murl/aurora/Plugin.java
@@ -1,25 +1,38 @@
 package digital.murl.aurora;
 
-import com.alibaba.fastjson.JSON;
-import digital.murl.aurora.commands.TestCommand;
-import digital.murl.aurora.effects.Effect;
+import digital.murl.aurora.commands.PointAddCommand;
+import digital.murl.aurora.commands.PointCommand;
+import digital.murl.aurora.points.Points;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.tozymc.spigot.api.command.CommandController;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import java.io.IOException;
 
 public final class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        Aurora.plugin = this;
         Aurora.logger = getLogger();
         Aurora.logger.info("it works!");
-        Aurora.commandController = new CommandController(this);
-        Aurora.commandController.addCommand(new TestCommand());
 
-        JSON.parseObject()
+        PointCommand pointCommand = new PointCommand();
+        PointAddCommand pointAddCommand = new PointAddCommand(pointCommand);
+
+        Aurora.commandController = new CommandController(this);
+        Aurora.commandController.addCommand(pointCommand);
+        Aurora.commandController.addCommand(pointAddCommand);
+
+        if (getDataFolder().mkdirs()) {
+
+        }
+
+        try {
+            Points.load();
+        } catch (IOException e) {
+            Aurora.logger.severe("Failed to load points: " + e.getMessage());
+        }
     }
 
     @Override
