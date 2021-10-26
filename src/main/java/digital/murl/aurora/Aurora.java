30ade592ab5015d4b44192cead22354b61efa0f2
Joe Sorensen <hello@murl.digital>
2021-10-26 09:21:31 -0600

    modified executeAgentAction

--- src/main/java/digital/murl/aurora/Aurora.java
+++ src/main/java/digital/murl/aurora/Aurora.java
@@ -21,16 +21,8 @@ public class Aurora {
     }
 
     public static void executeAgentAction(String agentName, String actionName, HashMap<Object, Object> params) {
-        Agent agent = null;
-        try {
-            agent = AgentManager.getAgent(agentName);
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-            return;
-        }
-        HashMap<String, Action> actions = Objects.requireNonNull(agent).getActions();
-        if (actions.containsKey(actionName)) {
-            actions.get(actionName).apply(agent, params);
-        }
+        if (!plugin.isEnabled()) return;
+
+        AgentManager.executeAgentAction(agentName, actionName, params);
     }
 }
