455ea2955268712707e5ba9fab5865036104bd0e
Joe Sorensen <hello@murl.digital>
2021-10-26 09:20:31 -0600

    added AgentRegistrar

--- src/main/java/digital/murl/aurora/Aurora.java
+++ src/main/java/digital/murl/aurora/Aurora.java
@@ -3,21 +3,21 @@ package digital.murl.aurora;
 import com.dslplatform.json.DslJson;
 import digital.murl.aurora.agents.Agent;
 import digital.murl.aurora.agents.AgentManager;
+import digital.murl.aurora.agents.AgentRegistrar;
 import xyz.tozymc.spigot.api.command.CommandController;
 
 import java.util.HashMap;
-import java.util.Objects;
 import java.util.logging.Logger;
 
-public class Aurora<T> {
+public class Aurora {
     public static Logger logger;
     public static Plugin plugin;
     public static DslJson<Object> dslJson;
 
     static CommandController commandController;
 
-    public static <T extends Agent> void registerAgent(String name, Class<T> agent) {
-        AgentManager.addAgent(name, agent);
+    public static <T extends Agent> void registerAgent(String name, Class<T> agent) throws Exception {
+        AgentRegistrar.addAgent(name, agent);
     }
 
     public static void executeAgentAction(String agentName, String actionName, HashMap<Object, Object> params) {
