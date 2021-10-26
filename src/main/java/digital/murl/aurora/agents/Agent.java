ac757afeea8039f0a2e1777ef91591f640ef2950
Joe Sorensen <hello@murl.digital>
2021-10-26 09:19:43 -0600

    added init and cleanup methods to Agent

--- src/main/java/digital/murl/aurora/agents/Agent.java
+++ src/main/java/digital/murl/aurora/agents/Agent.java
@@ -8,4 +8,7 @@ public abstract class Agent {
     public boolean isEphemeral = false;
 
     public abstract HashMap<String, Action> getActions();
+
+    public abstract void init(HashMap<Object, Object> params);
+    public abstract void cleanup();
 }
