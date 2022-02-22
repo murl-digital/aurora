958a6d4b895b1afbfb04860889a809770479c746
Joe Sorensen <hello@murl.digital>
2022-02-22 13:42:17 -0700

    added first agent related methods for executing actions

--- src/main/java/digital/murl/aurora/agents/AgentManager.java
+++ src/main/java/digital/murl/aurora/agents/AgentManager.java
@@ -1,44 +1,111 @@
 package digital.murl.aurora.agents;
 
-import digital.murl.aurora.Action;
+import com.google.common.base.Throwables;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import digital.murl.aurora.Aurora;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class AgentManager {
-    private static final ConcurrentHashMap<Integer, Agent> activeAgents;
+    private static final ConcurrentHashMap<String, Agent> activeAgents;
+    private static final Cache<String, Agent> agentCache;
 
     static {
         activeAgents = new ConcurrentHashMap<>();
+        agentCache = CacheBuilder.newBuilder()
+            .expireAfterAccess(60, TimeUnit.SECONDS)
+            .build();
     }
 
-    public static void executeAgentAction(String agentName, String actionName, HashMap<Object, Object> params) {
+    @Nullable
+    public static String createAgent(String id, String agentName, Map<String, Object> params) {
+        Agent agent = getInstance(agentName);
+        if (agent == null) return null;
+
+        agent.init(params);
+        activeAgents.put(id, agent);
+
+        return id;
+    }
+
+    @Nullable
+    public static String createAgent(String agentName, Map<String, Object> params) {
+        Agent agent = getInstance(agentName);
+        if (agent == null) return null;
+
+        agent.init(params);
+        String id = generateRandomString();
+        activeAgents.put(id, agent);
+
+        return id;
+    }
+
+    public static String executeAgentAction(String agentName, String actionName, Map<String, Object> params) {
+        Agent agent = getInstance(agentName);
+        Map<String, Action> actions = AgentRegistrar.getAgentActions(agentName);
+        if (agent == null || actions == null)
+            return null;
+        if (!actions.containsKey(actionName))
+            return null;
+
+        CacheBehavior cacheBehavior = AgentRegistrar.getCacheBehavior(agentName);
+
+        String id = "";
+        agent.init(params);
+        boolean keep = actions.get(actionName).apply(agent, params);
+
+        switch (Objects.requireNonNull(cacheBehavior)) {
+            case EPHEMERAL:
+                break;
+            case PERSISTENT:
+                id = generateRandomString();
+                activeAgents.put(id, agent);
+                break;
+            case NORMAL:
+                id = generateRandomString();
+                if (keep)
+                    activeAgents.put(id, agent);
+                else
+                    agentCache.put(id, agent);
+        }
+
+        return id;
+    }
+
+    @Nullable
+    private static Agent getInstance(String agentName) {
         Agent agent;
         try {
             agent = AgentRegistrar.getAgent(agentName);
         } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-            return;
+            Aurora.logger.severe(Throwables.getStackTraceAsString(e));
+            return null;
         }
-        if (agent == null) return;
 
-        agent.init(params);
-        runAction(actionName, params, agent, AgentRegistrar.getAgentActions(agentName));
+        if (agent == null) {
+            Aurora.logger.warning(String.format("Attempted to create an agent of type %s that doesn't exist", agentName));
+            return null;
+        }
+        return agent;
     }
 
-    private static void runAction(String actionName, HashMap<Object, Object> params, Agent agent, HashMap<String, Action> actions) {
-        // TODO: replace with InvalidArgumentException
-        if (actions == null) return;
+    // https://medium.com/beingcoders/ways-to-generate-random-string-in-java-6d3b1d964c02
+    private static String generateRandomString() {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
 
-        if (actions.containsKey(actionName)) {
-            actions.get(actionName).apply(agent, params);
-        }
-    }
-
-    public static void executeAgentAction(int agentId, String actionName, HashMap<Object, Object> params) {
-        if (!activeAgents.containsKey(agentId)) return;
-
-        Agent agent = activeAgents.get(agentId);
-        runAction(actionName, params, agent, AgentRegistrar.getAgentActions(agent.getClass()));
+        return random.ints(leftLimit, rightLimit + 1)
+            .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+            .limit(targetStringLength)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
     }
 }
