1e5c48f596b71998fb58922abfe7ac241b4f161c
Joe Sorensen <hello@murl.digital>
2022-02-28 09:12:31 -0700

    remove unnecessary public abstract in agent

--- aurora/src/main/java/digital/murl/aurora/agents/Agent.java
+++ aurora/src/main/java/digital/murl/aurora/agents/Agent.java
@@ -3,6 +3,6 @@ package digital.murl.aurora.agents;
 import java.util.Map;
 
 public interface Agent {
-    public abstract void init(Map<String, Object> params);
-    public abstract void cleanup();
+    void init(Map<String, Object> params);
+    void cleanup();
 }
